014a4c068f5cd4b37a4769c3cbca82540098100f
Nicolas BAIN <nicolas.bain@edu.devinci.fr>
2020-03-22 04:30:24 +0100

    3 more cities

--- meteo1.py
+++ meteo1.py
@@ -19,8 +19,23 @@ def pushtoMongo():
             'access_key': '547d7636a660c07121e7200da3f50b89',
             'query': 'London',
             }
+    params3 = {
+            'access_key': '547d7636a660c07121e7200da3f50b89',
+            'query': 'New York',
+            }
+    params4 = {
+            'access_key': '547d7636a660c07121e7200da3f50b89',
+            'query': 'Tokyo',
+            }
+    params5 = {
+            'access_key': '547d7636a660c07121e7200da3f50b89',
+            'query': 'Sydney',
+            }
     api_result = requests.get('http://api.weatherstack.com/current', params)
     api_result2 = requests.get('http://api.weatherstack.com/current', params2)
+    api_result3 = requests.get('http://api.weatherstack.com/current', params3)
+    api_result4 = requests.get('http://api.weatherstack.com/current', params4)
+    api_result5 = requests.get('http://api.weatherstack.com/current', params5)
     '''
     #here we can print some values returned by the api
     print(u'Current meteo at %s is :' % (api_response['location']['name']))
@@ -34,21 +49,35 @@ def pushtoMongo():
     client = MongoClient('localhost', 27017)
     data = json.loads("[" + api_result.text+"]")
     data2 = json.loads("[" + api_result2.text+"]")
-    print(data,data2)
+    data3 = json.loads("[" + api_result3.text+"]")
+    data4 = json.loads("[" + api_result4.text+"]")
+    data5 = json.loads("[" + api_result5.text+"]")
+    print(data,data2,data3,data4,data5)
     db = client.meteo
-    col = db.test
-    col2 = db.test2
+    paris = db.paris
+    london = db.london
+    new_york = db.new_york
+    tokyo = db.tokyo
+    sydney = db.sydney
+    
+    for x1 in data :
+        paris.insert_one(x1)
 
-    for x in data :
-           col.insert_one(x)
-
-
-    for z in data2 :
-           col2.insert_one(z)
-
-                
+    for x2 in data2 :
+        london.insert_one(x2)
+    
+    for x3 in data3 :
+        new_york.insert_one(x3)
+    
+    for x4 in data4 :
+        tokyo.insert_one(x4)
+    
+    for x5 in data5 :
+        sydney.insert_one(x5)
+           
     client.close()
     
+
     #print(api_response)
     
 
@@ -59,5 +88,5 @@ def foo():
 
 while True:
   foo()
-  time.sleep(360)
+  time.sleep(20)
 
